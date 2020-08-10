9b836becce143a6434a2a90f86f099f4a460250b
SamudraThio <samudra.thio@outlook.com>
2020-08-10 10:25:27 -0400

    Added Buying and Selling Functions.

--- code/Shop.cs
+++ code/Shop.cs
@@ -24,24 +24,68 @@ namespace SpaceGame
             Graphics.ShoppingBuyMenu();
             Console.Write("Make a selection:");
             Console.ReadLine();
+            
         }
         //-------------------------------------------------------------------
 
 
 
         // Stand in code for ShoppingBuyMenu
-        public static void BuyItem()
+        public static void DisplayInventory()
         {
-            Console.WriteLine("BuyItem function here");
+
+        }
+         
+        public static (decimal, int) BuyItem(decimal x, decimal y, int z)
+        {
+            decimal money = x;
+            decimal buyPrice = y;
+
+            int capacity = z;
+
+            try
+            {
+                if (capacity == 10)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                money = money - buyPrice;
+
+                if (money < 0)
+                {
+                    money += buyPrice;
+                    throw new InputOutofBounds();
+                }
+                capacity++;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("The ship is at max capacity.");
+            }
+            catch (InputOutofBounds)
+            {
+                Console.WriteLine("You don't have sufficient funds.");
+            }
+
+            return (money, capacity);
         }
 
-        public static void SellItem()
+
+        public static (decimal, int) SellItem(decimal x, decimal y, int z)
         {
-            Console.WriteLine("SellItem function here");
+            decimal money = x;
+            decimal sellPrice = y;
+
+            int capacity = z;
+
+            money += sellPrice;
+            capacity--;
+
+            return (money, capacity);
         }
     }
 
-    // Items are marked with the name and price
     public class Item
     {
         string itemName;
@@ -106,85 +150,13 @@ namespace SpaceGame
             return middleEarthStore;
         }
     }
-}
-
-
-
-
-
-
-
-
-
-/* This class is for the shop on each planet and the trading system
-
-public class Shop
-{
-    public decimal totalMoney; // Can we pull this from Character class?
-
-    // Sets the name of the Merchant 
-    public string MerchantName
+    class InputOutofBounds : Exception
     {
-        get;
-        set;
-    }
+        public InputOutofBounds()
+        {
 
-    // Sets the store inventory
-    public class StoreInventory
-    {
-        List<string> storeInventory = new List<string>();
-        storeInventory.Add("Water");
-        storeInventory.Add("Fuel");
-        storeInventory.Add("Oxygen");
-
-    }
-
-    // Sets the name of the Merchant and assigns the items they'll sell
-    public Shop(string name)
-    {
-        MerchantName = name;
-        storeInventory.Add(Water);
-        storeInventory.Add(Fuel);
-        storeInventory.Add(Oxygen);
-    }
-
-    public void BuyItem(StoreItems item)
-    {
-
-        totalMoney = totalMoney - itemCost;
-        Inventory.Add(item);
-        Console.WriteLine("You bought {description} for {itemCost}.");
-    }
-
-    public void SellItem(StoreItems item)
-    {
-        totalMoney = totalMoney + itemCost;
-        Inventory.Remove(item);
-        Console.WriteLine("You sold {description} for {itemCost}");
-
-    }
-}
-public class List
-{ }
-
-public class PlanetShop
-{
-    List<Shop> shopkeepers = new List<Shop>();
-
-    public PlanetShop()
-    {
-        Shop john = new Shop("John");
-        john.AddItemToInventory();
-
-        Shop cody = new Shop("Cody");
-        cody.AddItemToInventory();
-
-        Shop sam = new Shop("Sam");
-        sam.AddItemToInventory();
-
-        shopkeepers.add();
+        }
     }
 }
 
-*/
 
